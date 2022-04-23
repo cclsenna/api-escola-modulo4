2d026d81386a2433d5837bbdd8e78e589f06ac24
unknown <rwps1234@gmail.com>
2022-04-22 23:07:38 -0300

    criando classe AlunoModel.js

---
+++ src/models/AlunoModel.js
@@ -0,0 +1,11 @@
+
+class AlunoModel {
+    constructor (nome, sobrenome, datanascimento) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.datanascimento = datanascimento
+    }
+    
+}
+
+export default AlunoModel;

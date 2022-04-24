6e8b0cdc5253538f6130daba095e4e1775a6fd34
unknown <rwps1234@gmail.com>
2022-04-24 20:04:55 -0300

    adicionando rota post

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ import AlunoController from "../controllers/AlunoController.js";
 
 const router = (app) => {
   app.get("/aluno", AlunoController.exibirAlunos);
-  
+  app.post("/aluno", AlunoController.cadastrar);
 };
 
 export default router;

89d9dd280d530dc7121cd01d869f216a26757105
unknown <rwps1234@gmail.com>
2022-04-24 10:41:01 -0300

    criando metodo exibirAlunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,17 +1,18 @@
 import AlunoModel from "../models/AlunoModel.js"
-import bancoTeste from "../infra/db.js"
+import AlunoDao from "../DAO/AlunoDAO.js"
+import db from "../infra/createDb.js"
 
 class AlunoController {
+    
     static exibirAlunos = (req,res) => {
-       return res.status(200).json(bancoTeste.alunos)
+        const alunoDao = new AlunoDao(db)
+        alunoDao.lista()
+        .then((aluno) => {
+            res.json(aluno)
+        }).catch((erro)=> {console.log(erro)})
     }
 
-    static cadastrarAluno = (req,res) => {
-        const body = req.body;
-        const insereAluno = new AlunoModel(body.nome, body.sobrenome, body.dataNascimento);
-        bancoTeste.alunos.push(insereAluno);
-        res.status(201).json(`Aluno: ${body.nome} cadastrado com sucesso.`) 
-    }
+    
 
 }
 

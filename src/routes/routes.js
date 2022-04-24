5c866db2903f7cc24970d52a224ac1435a9b3b9f
unknown <rwps1234@gmail.com>
2022-04-24 10:36:38 -0300

    adicionando metodo get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ import AlunoController from "../controllers/AlunoController.js";
 
 const router = (app) => {
   app.get("/aluno", AlunoController.exibirAlunos);
-  app.post("/aluno", AlunoController.cadastrarAluno);
+  
 };
 
 export default router;

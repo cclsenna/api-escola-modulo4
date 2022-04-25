4d23ab96d0c02876b082ac2faa4d05a9d821c64f
unknown <rwps1234@gmail.com>
2022-04-25 17:48:07 -0300

    adicionando método excluir

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,43 +1,67 @@
-import AlunoModel from "../models/AlunoModel.js"
-import AlunoDao from "../DAO/AlunoDAO.js"
-import db from "../infra/createDb.js"
+import AlunoModel from "../models/AlunoModel.js";
+import AlunoDao from "../DAO/AlunoDAO.js";
+import db from "../infra/createDb.js";
 
 class AlunoController {
-    
-    static exibirAlunos = (req,res) => {
-        const alunoDao = new AlunoDao(db)
-        alunoDao.lista()
-        .then((aluno) => {
-            res.json(aluno)
-        }).catch((erro)=> {console.log(erro)})
-    }
-    static buscaAlunoPorId = (req,res) => {
-        const alunoDao = new AlunoDao(db)
-        const {id} = req.params
 
-        alunoDao.listaUm(id)
-        .then((resultado) => {
-            res.json(resultado)
-        }).catch((erro)=> {
-            console.log(erro)
-            res.json(erro)
-        })
-    }
-    static cadastrar = (req,res) => {
+  static exibirTodos = (req, res) => {
+    const alunoDao = new AlunoDao(db);
+    alunoDao
+      .lista()
+      .then((aluno) => {
+        res.json(aluno);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
 
-            const alunoDao = new AlunoDao(db)
-            const body = req.body;
-            const novoAluno = new AlunoModel(body.nome, body.sobrenome, body.dataNascimento);
-    
-            alunoDao.inserir(novoAluno)
-            .then((resultado)=> {
-                res.json(resultado)
-            }).catch((erro)=> {
-                res.json(erro)
-            })
-    }
-    
 
+  static exibirUmPorId = (req, res) => {
+    const alunoDao = new AlunoDao(db);
+    const { id } = req.params;
+
+    alunoDao
+      .listaUm(id)
+      .then((resultado) => {
+        res.json(resultado);
+      })
+      .catch((erro) => {
+        console.log(erro);
+        res.json(erro);
+      });
+  };
+
+
+  static cadastrar = (req, res) => {
+    const alunoDao = new AlunoDao(db);
+    const body = req.body;
+    const novoAluno = new AlunoModel(body.nome, body.sobrenome, body.dataNascimento);
+
+    alunoDao.inserir(novoAluno)
+      .then((resultado) => {
+        res.json(resultado);
+      })
+      .catch((erro) => {
+        res.json(erro);
+      });
+  };
+
+
+  static excluir = (req, res) => {
+    const alunoDao = new AlunoDao(db);
+    const { id } = req.params;
+
+    alunoDao.deletar(id)
+      .then((resultado) => {
+        res.json(resultado);
+      })
+      .catch((erro) => {
+        res.json(erro);
+      });
+  };
+
+  
 }
 
 export default AlunoController;

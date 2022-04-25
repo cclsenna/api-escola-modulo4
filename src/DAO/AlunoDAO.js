ad3b8b84dea1fd0ef13e7e82b4629412427b9306
unknown <rwps1234@gmail.com>
2022-04-24 23:07:11 -0300

    criando método listaUm

--- src/DAO/AlunoDAO.js
+++ src/DAO/AlunoDAO.js
@@ -1,45 +1,54 @@
 class AlunoDao {
+  constructor(db) {
+    this._db = db;
+  }
 
-    constructor(db) {
-        this._db = db;
-    }
+  async lista() {
+    return await new Promise((resolve, reject) => {
+      const query = "SELECT * FROM alunos";
 
-    
-     async lista () {
-        return await new Promise ((resolve, reject) => {
-            const query = 'SELECT * FROM alunos'
-
-            this._db.all(query, (erro, resultado) => {
-                if(!erro) {
-                    return resolve(resultado)
-                } else {
-                    console.log(erro);
-                    return reject('Não foi possivel listar aluno')
-                }
-            })    
-        })
+      this._db.all(query, (erro, resultado) => {
+        if (!erro) {
+          return resolve(resultado);
+        } else {
+          console.log(erro);
+          return reject("Não foi possivel listar aluno");
         }
-      async inserir (aluno) {
-        return await new Promise ((resolve, reject) => {
-            
-            const query = `INSERT INTO alunos (nome,sobrenome,dataNascimento) values (?,?,?)`
+      });
+    });
+  }
 
-            this._db.run(query,[aluno._nome, aluno._sobrenome, aluno._dataNascimento], 
-                (erro, resultado) => {
+  async listaUm(id) {
+    return await new Promise((resolve, reject) => {
+      const query = "SELECT * FROM alunos WHERE id = ?";
 
-                if(!erro) {
-                    return resolve('Aluno cadastrado com sucesso')
-                } else {
-                    console.log(erro);
-                    return reject('Não foi possivel cadastrar aluno')
-                }
-            })    
-        
-        })
+      this._db.get(query, [id], (erro, resultado) => {
+        if (!erro) {
+          return resolve(resultado);
+        } else {
+          return reject("Aluno não encontrado");
         }
-            
-        
+      });
+    });
+  }
+
+  async inserir(aluno) {
+    return await new Promise((resolve, reject) => {
+      const query = `INSERT INTO alunos (nome,sobrenome,dataNascimento) values (?,?,?)`;
+
+      this._db.run(
+        query, [aluno._nome, aluno._sobrenome, aluno._dataNascimento],
+        (erro, resultado) => {
+          if (!erro) {
+            return resolve("Aluno cadastrado com sucesso");
+          } else {
+            console.log(erro);
+            return reject("Não foi possivel cadastrar aluno");
+          }
+        }
+      );
+    });
+  }
 }
-
 
 export default AlunoDao;

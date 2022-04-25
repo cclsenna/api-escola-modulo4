f133fe8e9bba0a0cf81096a131d3ba5a82098c7e
unknown <rwps1234@gmail.com>
2022-04-24 23:05:34 -0300

    adicionando nova rota get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import AlunoController from "../controllers/AlunoController.js";
 
 const router = (app) => {
   app.get("/aluno", AlunoController.exibirAlunos);
+  app.get("/aluno/:id", AlunoController.buscaAlunoPorId)
   app.post("/aluno", AlunoController.cadastrar);
 };
 

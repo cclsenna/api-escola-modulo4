ba338bba7ed3b7280a078a63d18c3389eda74035
unknown <rwps1234@gmail.com>
2022-04-27 20:08:51 -0300

    adicionando método validaDados

--- src/models/AlunoModel.js
+++ src/models/AlunoModel.js
@@ -1,11 +1,25 @@
+import Validator from "fastest-validator";
 
 class AlunoModel {
-    constructor (nome, sobrenome, dataNascimento) {
-        this._nome = nome;
-        this._sobrenome = sobrenome;
-        this._dataNascimento = dataNascimento;
-    }
-    
+  constructor(nome, sobrenome, dataNascimento, turma) {
+    this._nome = nome;
+    this._sobrenome = sobrenome;
+    this._dataNascimento = dataNascimento;
+    this._turma = turma;
+  }
+
+  static validaDados(body) {
+    const schema = {
+      nome: { type: "string", optional: false },
+      sobrenome: { type: "string", optional: false },
+      dataNascimento: { type: "string", optional: false },
+      turma: { type: "number", optional: false },
+    };
+
+    const validator = new Validator();
+
+    return validator.validate(body, schema);
+  }
 }
 
 export default AlunoModel;

b00f7a4bf9ae5c35f64335f3c87a821d12bed4f6
Caio Senna <caiosenna93@gmail.com>
2022-04-29 10:21:10 -0300

    adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import {routerAluno,RouterProfessor} from "./routes/routes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 routerAluno(app);
 RouterProfessor(app);
 

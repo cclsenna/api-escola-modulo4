6a965ac8279e782cfa57dc6f1b128e530951181f
Caio Senna <caiosenna93@gmail.com>
2022-06-04 17:14:18 -0300

    atualizando base de dados

--- src/infra/createDb.js
+++ src/infra/createDb.js
@@ -37,12 +37,25 @@ const ALUNOS_SCHEMA = `CREATE TABLE IF NOT EXISTS alunos (
 const INSERIR_ALUNOS = `
 INSERT INTO alunos(nome, sobrenome, dataNascimento,tel,email,foto,id_professor,turno,nota_historia,nota_matematica,nota_geografia,nota_portugues,nota_artes,nota_Edfisica,cep,rua,bairro,cidade,uf,numero)
 VALUES 
-    ('Iata','Anderson', '1991/01/15','219999999',"teste@teste.com.br",null ,'1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
-    ('Karla', 'Valeriano', '1993/10/13','219999999',"teste@teste.com.br",null,'1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
-    ('Giovanni', 'Clayton', '1994/07/2','219999999',"teste@teste.com.br",null,'1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
-    ('Edu', 'Moreira', '1991/05/20','219999999',"teste@teste.com.br",null,'1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
-    ('Rodrigo', 'William', '1995/06/18','219999999',"teste@teste.com.br",null,'1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
-    ('Paulo', 'Reis', '1995/01/13','219999999',"teste@teste.com.br",null,'1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43");
+    ('Iata','Anderson', '1991/01/15','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png' ,'1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Karla', 'Valeriano', '1993/10/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Giovanni', 'Clayton', '1994/07/2','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Edu', 'Moreira', '1991/05/20','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Rodrigo', 'William', '1995/06/18','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Paulo', 'Reis', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Thereza', 'Mello', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Eduardo', 'Moreira', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Maria', 'Rita', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Gabriel', 'Lopes', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('William', 'Firmino', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Eliane', 'Cristina', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Milla', 'Rany', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Kassiane', 'Façanha', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Inara', 'X', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43"),
+    ('Douglas', 'adnreade', '1995/01/13','219999999',"teste@teste.com.br",'https://www.pluriconsultoria.com.br/wp-content/uploads/2020/10/314197-1.png','1','M','9.5','9.5','9.5','9.5','9.5','9.5',"22222","rua teste","bairro teste","testópolis","TS","43");
+
+    
+
 
 `
 
